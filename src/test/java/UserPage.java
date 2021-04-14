3895367ef9560be43e5e2655f6bf5fcc0623dc4f
semenantufiev <alexantufiev@gmail.com>
2021-04-14 20:42:00 +0300

    Добавил локатор для иконки друзей

--- src/test/java/UserPage.java
+++ src/test/java/UserPage.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
 
 public class UserPage {
+    private static final String LOCATOR_FRIENDS_ICON = ".//div[@data-module='ToolbarManager']//li[@data-l='t,friends']";
     private WebDriver driver;
 
     public UserPage(WebDriver driver) {
